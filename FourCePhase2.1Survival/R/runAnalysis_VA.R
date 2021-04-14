bfd579d93fef57eddc8524e8380db1fb79118aea
ChuanHong <hong.chuan.hannah@gmail.com>
2021-04-14 04:37:18 +0000

    add VA functions

---
+++ FourCePhase2.1Survival/R/runAnalysis_VA.R
@@ -0,0 +1,11 @@
+runAnalysis_VA=function(currSiteId, dir.package, dir.output){
+  file.log=file(file.path(dir.output, paste0(currSiteId, "_log.txt")),open = "wt")
+  sink(file.log)
+  sink(file.log, type="message")
+  cat("TEMPORAL TREND ANALYSIS\n")
+  tryCatch(runAnalysis_TemporalTrend_VA(currSiteId, dir.package, dir.output),error=function(e) print(e))
+  cat("Phase1.1 PAPER REVISION\n")
+  tryCatch(runAnalysis_R1_VA(currSiteId, dir.package, dir.output),error=function(e) print(e))
+  sink(type = "message")
+  sink()
+}
