2013b8c6a2851faee9f9d94409de56f40d2482cc
ChuanHong <hong.chuan.hannah@gmail.com>
2021-04-14 18:22:28 +0000

    udpate nodocker

--- FourCePhase2.1Survival/example/example_docker.R
+++ FourCePhase2.1Survival/example/example_docker.R
@@ -1,4 +1,4 @@
 rm(list=ls())
 devtools::install_github("https://github.com/covidclinical/Phase2.1SurvivalRPackage", subdir="FourCePhase2.1Survival", upgrade=FALSE, ref="testing", force=T)
-runAnalysis()
-submitAnalysis()
+FourCePhase2.1Survival::runAnalysis()
+FourCePhase2.1Survival::submitAnalysis()
