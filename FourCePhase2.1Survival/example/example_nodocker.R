f9af6bcb0922e49adc53e18ea9c05bce0f12d627
ChuanHong <hong.chuan.hannah@gmail.com>
2021-04-14 18:04:49 +0000

    change ref

--- FourCePhase2.1Survival/example/example_nodocker.R
+++ FourCePhase2.1Survival/example/example_nodocker.R
@@ -1,5 +1,5 @@
 rm(list=ls())
-devtools::install_github("https://github.com/covidclinical/Phase2.1SurvivalRPackage", subdir="FourCePhase2.1Survival", upgrade=FALSE, ref="master", force=T)
+devtools::install_github("https://github.com/covidclinical/Phase2.1SurvivalRPackage", subdir="FourCePhase2.1Survival", upgrade=FALSE, ref="testing", force=T)
 library(FourCePhase2.1Data)
 library(FourCePhase2.1Survival)
 library(icd)
