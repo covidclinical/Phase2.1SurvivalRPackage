8550e99684756cea02380f5e322b30b04bbf8c66
Chuan <hong.chuan.hannah@gmail.com>
2021-05-13 00:58:27 -0400

    update

--- FourCePhase2.1Survival/example/example_nodocker.R
+++ FourCePhase2.1Survival/example/example_nodocker.R
@@ -1,6 +1,6 @@
 rm(list=ls())
 remove.packages("FourCePhase2.1Survival")
-devtools::install_github("https://github.com/covidclinical/Phase2.1SurvivalRPackage", subdir="FourCePhase2.1Survival", upgrade=FALSE, ref="testing", force=T)
+devtools::install_github("https://github.com/covidclinical/Phase2.1SurvivalRPackage", subdir="FourCePhase2.1Survival", upgrade=FALSE, ref="master", force=T)
 currSiteId = "MGB" ## change to your siteid
 dir.input="/Users/chuanhong/Documents/Input"
 dir.output="/Users/chuanhong/Documents/Output"
