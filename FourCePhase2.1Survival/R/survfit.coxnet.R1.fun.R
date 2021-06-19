57d9c497ab1839667e5909c3900f1c4a3025ddc0
Chuan <hong.chuan.hannah@gmail.com>
2021-06-19 13:09:31 -0400

    update

--- FourCePhase2.1Survival/R/survfit.coxnet.R1.fun.R
+++ FourCePhase2.1Survival/R/survfit.coxnet.R1.fun.R
@@ -2,6 +2,9 @@ survfit.coxnet.R1.fun=function(dat.survival, nm.event, nm.lab.keep, nm.dem, nm.c
                             period.train, period.valid, calendar.date.cut,  t0.all, yes.cv=T, K=10, is.bt=T, method.impute="zero", myscale="original", is.ind=0, is.stand=0, mice.time, removeALT, include.dem=1, include.lab=1, include.cls=1){
   
   #cat("1. data preparing \n")
+  nm.dem=c("age_group_new", "sex", "race_new")
+  nm.dem.new=c("age", "sex", "race")
+  dat00=dat.prep.mice.scale.fun(dat.surv
   dat00=dat.prep.mice.scale.fun(dat.survival, nm.event, nm.dem, nm.lab.keep, nm.cls, method.impute, myscale, is.ind, mice.time)
   dat0=dat00
   if("ALT"%in%colnames(dat0) & "AST"%in%colnames(dat0)){
